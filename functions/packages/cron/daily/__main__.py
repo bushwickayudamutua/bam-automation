4a620d50073591f1531378c263d0af697acf1afc
Brian <brian@abelson.live>
2024-12-28 09:47:41 -0500

    remove unused import

--- functions/packages/cron/daily/__main__.py
+++ functions/packages/cron/daily/__main__.py
@@ -2,9 +2,6 @@ from bam_core.functions.base import Function
 from bam_core.functions.dedupe_airtable_views import DedupeAirtableViews
 from bam_core.functions.update_mailjet_lists import UpdateMailjetLists
 from bam_core.functions.snapshot_airtable_views import SnapshotAirtableViews
-from bam_core.functions.analyze_fulfilled_requests import (
-    AnalyzeFulfilledRequests,
-)
 
 
 def main(event, context):
