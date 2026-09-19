637067b460d8603f1d52b2212fd6b104aa2a0d56
Zakieh Tayyebi <zakieh@mac.mynetworksettings.com>
2026-09-18 21:38:00 -0400

    exclude texted today by default

--- core/bam_core/functions/send_dialpad_sms_v2.py
+++ core/bam_core/functions/send_dialpad_sms_v2.py
@@ -23,6 +23,12 @@ class SendDialpadSMSV2(Function):
             description="The template of the message to send via SMS. Use [FIRST_NAME] to insert the first name and [REQUEST_URL] to insert a request form URL which is randomized so it wont get blocked by Dialpad.",
         ),
         Param(
+            name="exclude_texted_today",
+            type="bool",
+            default=True,
+            description="If true, households that were texted today will be excluded.",
+        ),
+        Param(
             name="max_messages",
             type="int",
             default=None,
@@ -43,12 +49,18 @@ class SendDialpadSMSV2(Function):
         """
         view_name = params.get("view_name")
         message = params.get("message_template")
-        max_messages = params.get("max_messages") or 1e9
+        exclude_texted_today = params.get("exclude_texted_today", True)
+        max_messages = params.get("max_messages") or 500
         dry_run = params.get("dry_run", True)
 
+        households_formula = "NOT(IS_SAME({Last Texted}, TODAY()))" if exclude_texted_today else None
+        households_all = Household.all(view=view_name, formula=households_formula, max_records=max_messages)
+
+        self.log.info(f"Selected {len(households_all)} households!")
+        
         num_messages_sent = 0
         for household in self.dialpad.send_sms_v2(
-            households=Household.all(view=view_name, max_records=max_messages),
+            households=households_all,
             message=message,
             testing=dry_run
         ):
@@ -60,6 +72,8 @@ class SendDialpadSMSV2(Function):
                 self.log.info(f"Setting Last Texted for household {household.bam_id}")
                 household.last_texted = now_est().date()
                 household.save()
+        
+        self.log.info(f"Successfully sent {num_messages_sent} messages!")
 
 
 if __name__ == "__main__":
